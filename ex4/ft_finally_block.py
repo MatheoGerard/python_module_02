e14103db51f94daae89d378797a5f93b7bb7ecb0
Matheo Gerard <mgerard@fu-r4-p7.s19.be>
2026-05-28 20:57:56 +0200

    add typing in ex4

--- ex4/ft_finally_block.py
+++ ex4/ft_finally_block.py
@@ -20,7 +20,7 @@ def water_plant(plant_name: str) -> None:
         raise PlantError(f"Invalid plant name to water: '{plant_name}'")
 
 
-def test_watering_system():
+def test_watering_system() -> None:
     print("Testing valid plants...")
     print("Opening watering system")
     try:
